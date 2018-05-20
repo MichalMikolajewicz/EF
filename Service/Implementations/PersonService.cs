df058420c3283da6eb337f9dc05891390b054150
Michal Mikolajewicz <michal.mikolajewicz@gmail.com>
2018-05-20 17:05:32 +0200

    Additional Projects and dependencies

---
+++ Service/Implementations/PersonService.cs
@@ -0,0 +1,20 @@
+﻿using Data.Core;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Implementations
+{
+	public class PersonService : IPersonService
+	{
+		private readonly IUnitOfWork unitOfWork;
+
+		public PersonService(IUnitOfWork unitOfWork)
+		{
+			this.unitOfWork = unitOfWork;
+		}
+
+		
+	}
+}
