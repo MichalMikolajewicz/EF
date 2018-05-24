7ebaf04d3aa30d28629f57ec70046796c23cc1fb
Michal Mikolajewicz <michal.mikolajewicz@gmail.com>
2018-05-24 20:28:50 +0200

    tests modification

--- DataTests/PersonRepositoryTests.cs
+++ DataTests/PersonRepositoryTests.cs
@@ -13,31 +13,27 @@ namespace DataTests
 {
 	public class PersonRepositoryTests
 	{
-		//How to improve this?
 		[Fact]
 		public void Should_Run_GetAll()
+		{
+			//Arrange
+			var optionsBuilder = Setup();
+			var context = new AdventureWorks2014Context(optionsBuilder.Options);
+			var personRepository = new PersonRepository(context);
+
+			//Act
+			var result = personRepository.GetAll();
+
+			//Assert
+			Assert.NotNull(result);
+		}
+
+		private DbContextOptionsBuilder<AdventureWorks2014Context> Setup()
 		{
 			var optionsBuilder = new DbContextOptionsBuilder<AdventureWorks2014Context>();
 			var connection = @"Server=MICHAL\SQLEXPRESS;Database=AdventureWorks2014;Trusted_Connection=True;";
 			optionsBuilder.UseSqlServer(connection);
-
-			var contextMock = new Mock<AdventureWorks2014Context>(optionsBuilder.Options);
-			var people = new List<Person> { new Person { BusinessEntityId = 1 } };
-
-			var queryable = people.AsQueryable();
-
-			var dbSet = new Mock<DbSet<Person>>();
-			dbSet.As<IQueryable<Person>>().Setup(m => m.Provider).Returns(queryable.Provider);
-			dbSet.As<IQueryable<Person>>().Setup(m => m.Expression).Returns(queryable.Expression);
-			dbSet.As<IQueryable<Person>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
-			dbSet.As<IQueryable<Person>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
-
-			contextMock.Setup(x => x.Set<Person>()).Returns(dbSet.Object);
-			var personRepository = new PersonRepository(contextMock.Object);
-
-			var result = personRepository.GetAll();
-
-			contextMock.Verify(x => x.Set<Person>(), Times.Once);
+			return optionsBuilder;
 		}
 	}
 }
